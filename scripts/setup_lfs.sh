e522a61be302280dcb54d0973887f7dfc3af3890
Mohamed Nour <derbelnourmohamed@gmail.com>
2024-04-30 20:08:17 +0100

    This script will run all the scripts

---
+++ scripts/setup_lfs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# This script will run all the scripts in the order they are listed.
+
+# Ensure the script is run as root
+if [ "$(id -u)" -ne 0 ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Install Host System Requirements 
+echo "Running 2-check_and_install.sh..."
+./2-check_and_install.sh
+
+# create partitions and mount the LFS file system
+echo "Running 2-check_and_install.sh..."
+./0-create_partitions.sh
+
+# Set the LFS variable
+export LFS="/mnt/lfs"
+
+# Check if the LFS directory exists
+if [ ! -d "$LFS" ]; then
+    echo "The LFS directory does not exist. Please create it before running this script."
+    exit 1
+fi
+
+# downlod and verify the LFS sources code
+echo "Running 3-download_and_verify.sh..."
+./3-download_and_verify.sh
+
+# Setup the LFS directories
+echo "Running 4-setup_lfs_directories.sh..."
+./4-setup_lfs_directories.sh
+
+echo "All scripts have been executed successfully."
