0bef5988b0dd70a89771a6b55ae1f922b23178aa
Mohamed Nour <derbelnourmohamed@gmail.com>
2024-04-30 23:37:21 +0100

    emplement lfs user and setup the env

--- scripts/setup_lfs.sh
+++ scripts/setup_lfs.sh
@@ -9,10 +9,16 @@ fi
 
 # Install Host System Requirements 
 echo "Running 2-check_and_install.sh..."
+
+sleep 2
+
 ./2-check_and_install.sh
 
 # create partitions and mount the LFS file system
 echo "Running 2-check_and_install.sh..."
+
+sleep 2
+
 ./0-create_partitions.sh
 
 # Set the LFS variable
@@ -26,10 +32,31 @@ fi
 
 # downlod and verify the LFS sources code
 echo "Running 3-download_and_verify.sh..."
+
+sleep 2
+
 ./3-download_and_verify.sh
 
 # Setup the LFS directories
 echo "Running 4-setup_lfs_directories.sh..."
+
+sleep 2
+
 ./4-setup_lfs_directories.sh
+
+# sets up the LFS user
+echo "sets up the LFS user"
+
+sleep 2
+
+./5-setup_lfs_user.sh
+
+# setup the LFS user's environment
+echo "Setting up lfs user's environment..."
+
+sleep 2
+
+./6-setup_lfs_profile.sh
+
 
 echo "All scripts have been executed successfully."
