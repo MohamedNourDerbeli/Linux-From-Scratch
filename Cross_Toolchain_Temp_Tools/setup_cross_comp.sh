509acf924c39f70e2556a1b07e895e0bda78c193
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-05-12 14:39:04 +0100

    feat(setup): automate component setup and cleanup

---
+++ Cross_Toolchain_Temp_Tools/setup_cross_comp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Run the binutils setup script
+$LFS/usr/1-binutils_setup.sh
+
+# Run the GCC setup script
+$LFS/usr/2-GCC_setup.sh
+
+# Run the Linux API headers setup script
+$LFS/usr/3-Linux_API_Headers.sh
+
+# Run the Glibc setup script
+$LFS/usr/4-Glibc_setup.sh
+
+# Run the Libstdc++ setup script
+$LFS/usr/5-Libstdc++.sh
+
+# Run the M4 setup script
+$LFS/usr/6-M4.sh
+
+# Remove the setup scripts
+rm -f $LFS/usr/*.sh
+
