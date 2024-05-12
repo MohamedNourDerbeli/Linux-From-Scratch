c7352890756ceef42c16286f8fee3e5da906081e
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-05-12 23:03:12 +0100

    feat(build): automate Gawk installation in LFS setup

---
+++ Cross_Toolchain_Temp_Tools/13-Gawk.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Change to the LFS/sources directory
+cd $LFS/sources
+
+# Extract Gawk from the tar.xz file
+echo "Extracting Gawk"
+tar -xvJf gawk-5.3.0.tar.xz
+
+# Change to the extracted Gawk directory
+cd $LFS/sources/gawk-5.3.0
+
+# Remove the 'extras' target from Makefile.in
+sed -i '/extras//' Makefile.in
+
+# Configure and build Gawk with specified parameters
+echo "Configuring and building Gawk"
+time ./configure --prefix=/usr   \
+            --host=$LFS_TGT \
+            --build=$(build-aux/config.guess) && make && make DESTDIR=$LFS install
+
+# Change back to the LFS/sources directory
+cd $LFS/sources
+
+# Remove the extracted Gawk directory to save space
+rm -rf gawk-5.3.0
