09f4c8968fa774d6ee8cb47e432d5798341213bd
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-05-12 19:13:54 +0100

    refactor(setup): add delay before executing setup scripts

--- Cross_Toolchain_Temp_Tools/setup_cross_comp.sh
+++ Cross_Toolchain_Temp_Tools/setup_cross_comp.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Loop through scripts in numerical order
-for script in $(ls -v $LFS/usr/*.sh); do
+for script in $(ls -v $LFS/usr/*.sh | grep -E '/[0-9]+-'); do
+    echo "Start: $script"
+    sleep 5
     bash $script
 done
-
-# Remove the setup scripts
-rm -f $LFS/usr/*.sh
