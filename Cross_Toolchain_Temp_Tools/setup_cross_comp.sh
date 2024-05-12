ae6ce6a9fc7a4106c764d4d5f3f1e0142bc28d70
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-05-12 15:08:26 +0100

    refactor(setup): automate and order execution of setup scripts

--- Cross_Toolchain_Temp_Tools/setup_cross_comp.sh
+++ Cross_Toolchain_Temp_Tools/setup_cross_comp.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
 
-# Run the binutils setup script
-$LFS/usr/1-binutils_setup.sh
-
-# Run the GCC setup script
-$LFS/usr/2-GCC_setup.sh
-
-# Run the Linux API headers setup script
-$LFS/usr/3-Linux_API_Headers.sh
-
-# Run the Glibc setup script
-$LFS/usr/4-Glibc_setup.sh
-
-# Run the Libstdc++ setup script
-$LFS/usr/5-Libstdc++.sh
-
-# Run the M4 setup script
-$LFS/usr/6-M4.sh
-
-# Run the Ncurses setup script
-$LFS/usr/7-Ncurses.sh
+# Loop through scripts in numerical order
+for script in $(ls -v $LFS/usr/*.sh); do
+    bash $script
+done
 
 # Remove the setup scripts
 rm -f $LFS/usr/*.sh
