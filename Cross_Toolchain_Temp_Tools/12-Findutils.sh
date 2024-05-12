f4a8bd1cd8424937ef1dfe7869d5828305168d70
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-05-12 23:02:55 +0100

    feat(build): automate Findutils installation in LFS setup

---
+++ Cross_Toolchain_Temp_Tools/12-Findutils.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Change to the LFS/sources directory
+cd $LFS/sources
+
+# Extract Findutils from the tar.xz file
+echo "Extracting Findutils"
+tar -xvJf findutils-4.9.0.tar.xz
+
+# Change to the extracted Findutils directory
+cd $LFS/sources/findutils-4.9.0
+
+# Configure and build Findutils with specified parameters
+echo "Configuring and building Findutils"
+time ./configure --prefix=/usr                   \
+            --localstatedir=/var/lib/locate \
+            --host=$LFS_TGT                 \
+            --build=$(build-aux/config.guess) && make && make DESTDIR=$LFS install
+
+# Change back to the LFS/sources directory
+cd $LFS/sources
+
+# Remove the extracted Findutils directory to save space
+rm -rf findutils-4.9.0
