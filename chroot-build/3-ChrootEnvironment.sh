de0ef457e61a2d07a162e00e09e91006469f846a
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-05-17 12:56:15 +0100

    build(chroot-setup): Enter chroot environment for LFS setup

---
+++ chroot-build/3-ChrootEnvironment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export LFS="/mnt/lfs"
+
+
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    MAKEFLAGS="-j$(nproc)"      \
+    TESTSUITEFLAGS="-j$(nproc)" \
+    /bin/bash --login
