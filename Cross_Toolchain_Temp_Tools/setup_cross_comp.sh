e80ebb26daea12f91e83db8f81b5d8bb251ffffb
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 <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>
2024-05-18 15:55:16 +0100

    fix(bash): fix list file

--- Cross_Toolchain_Temp_Tools/setup_cross_comp.sh
+++ Cross_Toolchain_Temp_Tools/setup_cross_comp.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# Function to execute scripts
+run_script() {
+    if ! bash "$1"; then
+        echo "Failed to execute $1"
+        exit 1
+    fi
+}
+
 # Loop through scripts in numerical order
-for script in $(ls -v $LFS/usr/*.sh | grep -E '/[0-9]+-'); do
-    echo "Start: $script"
-    sleep 5
-    bash $script
+for script in $(ls -v $LFS/usr/*.sh); do
+    if [[ "$(basename "$script")" =~ ^[0-22]+- ]]; then
+        echo "Start: $script"
+        sleep 5
+        run_script "$script"
+    fi
 done
